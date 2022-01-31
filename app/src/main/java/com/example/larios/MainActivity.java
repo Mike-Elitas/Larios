129341ae6388f10a766bf12af6867c609297cf1f
Ivan Bosch <ivan.bosch.amengual@gmail.com>
2022-01-31 23:29:37 +0100

    v0.1

--- app/src/main/java/com/example/larios/MainActivity.java
+++ app/src/main/java/com/example/larios/MainActivity.java
@@ -1,37 +1,55 @@
 package com.example.larios;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
+//Clase de logIn
+public class MainActivity extends AppCompatActivity {
+    //suppressLint new api se usa para sortear un requerimento del codigo que no se puede contemplar en apis bajas
     @SuppressLint("NewApi")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Instanciacion de los elementos
         setContentView(R.layout.activity_main);
         Button button = findViewById(R.id.button);
         EditText user = findViewById(R.id.editTextTextPersonName);
         EditText pass = findViewById(R.id.editTextTextPassword);
-        button.setOnClickListener(view -> {
 
-            if (user.getText().toString().trim().equals("Paco") &&
-                    pass.getText().toString().trim().equals("1234")){
+
+        //Descomentar las dos lineas de abajo para crear los usuarios y volver a comentar o borrar una vez hecho
+        DBHelper dbHelper = new DBHelper(this);
+//        SQLiteDatabase database = dbHelper.getWritableDatabase();
+//        dbHelper.onCreate(database);
+
+        button.setOnClickListener(view -> {
+            //Uso de Query para validar usuario mas info en DBHelper>getUser()
+            if(dbHelper.getUser(user.getText().toString(), pass.getText().toString())){
+                startActivity(new Intent(this, MainActivity3.class));
+                button.setBackgroundColor(getColor(R.color.teal_200));
+                user.setHintTextColor(Color.BLACK);
+                pass.setHintTextColor(Color.BLACK);
+
+                //Temporal uso para modo camarero hasta que se implemente distincion de admin
+            } else if (user.getText().toString().equals("Paco2") && pass.getText().toString().equals("1234")){
                 startActivity(new Intent(this, MainActivity2.class));
                 button.setBackgroundColor(getColor(R.color.teal_200));
                 user.setHintTextColor(Color.BLACK);
                 pass.setHintTextColor(Color.BLACK);
+                //Cambio de interfaz para mostrar fallo de inicio de sesión
             } else {
                 user.setHintTextColor(Color.RED);
                 pass.setHintTextColor(Color.RED);
                 button.setBackgroundColor(Color.RED);
             }
+            //Reiniciar campos tras inicio o intento fallido de sesion
             user.setText("");
             pass.setText("");
 
--- app/src/main/java/com/example/larios/MainActivity2.java
+++ app/src/main/java/com/example/larios/MainActivity2.java
@@ -1,17 +1,16 @@
 package com.example.larios;
 
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.PopupMenu;
-import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+//Clase de menu de camarero
 public class MainActivity2 extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener {
 
 
@@ -19,8 +18,10 @@ public class MainActivity2 extends AppCompatActivity implements PopupMenu.OnMenu
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        //Boton de salida hacia el login (finaliza la pantalla)
         Button close = findViewById(R.id.appCompatButton_LogOut);
         close.setOnClickListener(v -> finish());
+        //Instancia de los botones que hacen de mesa
         Button table1 = findViewById(R.id.button1);
         Button table2 = findViewById(R.id.button2);
         Button table3 = findViewById(R.id.button3);
@@ -28,6 +29,7 @@ public class MainActivity2 extends AppCompatActivity implements PopupMenu.OnMenu
         Button table5 = findViewById(R.id.button5);
         Button table6 = findViewById(R.id.button6);
 
+        //Llamadas al metodo que muesta la información de las mesas
         table1.setOnClickListener(view -> createTablePopUp());
         table2.setOnClickListener(view -> createTablePopUp());
         table3.setOnClickListener(view -> createTablePopUp());
@@ -36,38 +38,56 @@ public class MainActivity2 extends AppCompatActivity implements PopupMenu.OnMenu
         table6.setOnClickListener(view -> createTablePopUp());
     }
 
+    /* FIXME: 31/01/2022 Añadir la funcionalidad completa a las mesas
+    -Gestion de la cuenta
+    -Pago
+    -Borrar elementos*/
+
+        //Este metodo crea un popUp con la informacion de las mesas (beta)
     public void createTablePopUp(){
+        //Instancia de la View que aloja la distribucion de las mesa
         AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
         final View popUp = getLayoutInflater().inflate(R.layout.table, null);
+
         Button close = popUp.findViewById(R.id.appCompatButton_Close);
         Button add = popUp.findViewById(R.id.appCompatButton_add);
         Button delete = popUp.findViewById(R.id.appCompatButton_Delete);
         Button pay = findViewById(R.id.appCompatButton_Pay);
+        //Creacion de un dialog (funciona como un popUp)
         dialogBuilder.setView(popUp);
         AlertDialog dialog = dialogBuilder.create();
         dialog.show();
+
+        //El boton close gestiona que se cierre el pop up al clicarse
         close.setOnClickListener(v -> dialog.dismiss());
+        //El boton add muestra un menu con los filtros de los elementos a añadir
         add.setOnClickListener(this::showPopUpTable);
     }
 
+    //Este metodo implementa el metodo onMenuItemClick para gestionar el item de este menu que se clica
     public void showPopUpTable(View view){
+        //Generacion de un menu con los items alojados en la layout popup_table
         PopupMenu popupMenu = new PopupMenu(this, view);
         popupMenu.setOnMenuItemClickListener(this);
         popupMenu.inflate(R.menu.popup_table);
         popupMenu.show();
     }
 
+    // FIXME: 31/01/2022 Asignar las query a cada opcion
+    //Este metodo gestiona la opcion seleccionada en y realiza el metodo asociado en el switch
     @Override
     public boolean onMenuItemClick(MenuItem item) {
+
+        //Este switch contiene los filtros de la carta
         switch (item.getItemId()){
             case R.id.Carnes:
-                Toast.makeText(this, "Carne", Toast.LENGTH_LONG).show();
+                Toast.makeText(this, "Carne", Toast.LENGTH_SHORT).show();
                 return true;
             case  R.id.Pescados:
-                Toast.makeText(this, "Pescado", Toast.LENGTH_LONG).show();
+                Toast.makeText(this, "Pescado", Toast.LENGTH_SHORT).show();
                 return true;
             case R.id.Bebidas:
-                Toast.makeText(this, "Bebidas", Toast.LENGTH_LONG).show();
+                Toast.makeText(this, "Bebidas", Toast.LENGTH_SHORT).show();
                 return true;
             default: return false;
         }
